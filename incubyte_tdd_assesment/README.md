46a063f4cec9b364e0e04ec894ad046d702ad266
Manuj <manuj26ptl@gmail.com>
2025-08-22 12:50:24 +0530

    chore: setup Ruby project with RSpec

---
+++ incubyte_tdd_assesment/README.md
@@ -0,0 +1,2 @@
+# incubyte_tdd_assesment
+TDD
---
+++ incubyte_tdd_assesment/spec/string_calculator_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "../string_calculator"
+
+RSpec.describe StringCalculator do
+  describe ".add" do
+  end
+end
