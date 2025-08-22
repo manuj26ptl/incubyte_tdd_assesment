338e6f27c0deabe12e2caa1313486b03f021d550
Manuj <manuj26ptl@gmail.com>
2025-08-22 16:44:48 +0530

    test: add test for negative number exception

--- incubyte_tdd_assesment/spec/string_calculator_spec.rb
+++ incubyte_tdd_assesment/spec/string_calculator_spec.rb
@@ -27,5 +27,10 @@ RSpec.describe StringCalculator do
     it "supports custom delimiters" do
       expect(StringCalculator.add("//;\n1;2")).to eq(3)
     end
+
+    it "throws exception with message for negative numbers" do
+      expect { StringCalculator.add("1,-2,3,-4") }
+        .to raise_error("negative numbers not allowed -2,-4")
+    end
   end
 end
