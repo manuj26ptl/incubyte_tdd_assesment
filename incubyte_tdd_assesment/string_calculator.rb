17ac73748b51234c9ddd45c4192a9cba1fd55149
Manuj <manuj26ptl@gmail.com>
2025-08-22 14:00:26 +0530

    feat: support newline as delimiter

--- incubyte_tdd_assesment/string_calculator.rb
+++ incubyte_tdd_assesment/string_calculator.rb
@@ -2,11 +2,11 @@
 
 class StringCalculator
   
-  DEFAULT_DELIMITER = /,/
+  DEFAULT_DELIMITER = /,|\n/
 
   def self.add(numbers)
     return 0 if numbers == ""
-    
+
     tokens = numbers.split(DEFAULT_DELIMITER)
     tokens.map!(&:to_i)
     tokens.sum
