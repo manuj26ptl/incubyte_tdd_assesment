9629fa6e0138db59af64eaf38e305b556153bdf1
Manuj <manuj26ptl@gmail.com>
2025-08-22 16:39:19 +0530

    feat: support configurable delimiter

--- incubyte_tdd_assesment/string_calculator.rb
+++ incubyte_tdd_assesment/string_calculator.rb
@@ -1,14 +1,25 @@
 # frozen_string_literal: true
 
 class StringCalculator
-  
   DEFAULT_DELIMITER = /,|\n/
 
   def self.add(numbers)
     return 0 if numbers == ""
 
-    tokens = numbers.split(DEFAULT_DELIMITER)
-    tokens.map!(&:to_i)
+    delimiter, body = parse_delimiter_and_body(numbers)
+
+    tokens = body.split(delimiter).map!(&:to_i)
     tokens.sum
   end
+
+  def self.parse_delimiter_and_body(input)
+    if input.start_with?("//")
+      header, body = input.split("\n", 2)
+      raw = header[2..] # everything after //
+      [Regexp.new(Regexp.escape(raw)), body]
+    else
+      [DEFAULT_DELIMITER, input]
+    end
+  end
+  private_class_method :parse_delimiter_and_body
 end
