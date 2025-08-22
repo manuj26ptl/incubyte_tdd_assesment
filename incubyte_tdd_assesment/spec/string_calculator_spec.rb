d04cce825f6da1207113b33beefe2b9d8b37365f
Manuj <manuj26ptl@gmail.com>
2025-08-22 14:29:28 +0530

    test: add test for custom delimiter

--- incubyte_tdd_assesment/spec/string_calculator_spec.rb
+++ incubyte_tdd_assesment/spec/string_calculator_spec.rb
@@ -23,5 +23,9 @@ RSpec.describe StringCalculator do
     it "handles new lines as well as commas" do
       expect(StringCalculator.add("1\n2,3")).to eq(6)
     end
+
+    it "supports custom delimiters" do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
   end
 end
