f34caf7de55c53dbae09a2087c60fa80410b2b33
Manuj <manuj26ptl@gmail.com>
2025-08-22 16:47:11 +0530

    optimize

--- incubyte_tdd_assesment/string_calculator.rb
+++ incubyte_tdd_assesment/string_calculator.rb
@@ -7,9 +7,8 @@ class StringCalculator
     return 0 if numbers == ""
 
     delimiter, body = parse_delimiter_and_body(numbers)
+    tokens = tokenize(body, delimiter)
 
-    tokens = body.split(delimiter).map!(&:to_i)
-    
     negatives = tokens.select(&:negative?)
     raise "negative numbers not allowed #{negatives.join(',')}" if negatives.any?
 
@@ -19,11 +18,16 @@ class StringCalculator
   def self.parse_delimiter_and_body(input)
     if input.start_with?("//")
       header, body = input.split("\n", 2)
-      raw = header[2..] # everything after //
+      raw = header[2..]
       [Regexp.new(Regexp.escape(raw)), body]
     else
       [DEFAULT_DELIMITER, input]
     end
   end
   private_class_method :parse_delimiter_and_body
+
+  def self.tokenize(body, delimiter)
+    body.split(delimiter).map!(&:to_i)
+  end
+  private_class_method :tokenize
 end
